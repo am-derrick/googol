a0d172ec115f29af2e4c4f62686c5df6bbdf8527
Derrick Ampire <ampire90@gmail.com>
2022-06-06 16:58:25 +0300

    videos results added

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -45,7 +45,7 @@ export const Results = () => {
           {results?.map(({ image, link: { href, title }}, index) => (
             <a className='sm:p-3 p-5' href={href} key={index} target='_blank' rel='noreferrer'>
               <img src={image?.src} alt={title} loading='lazy' />
-              <p className='w-36 break-words text-sm mt-2'>
+              <p className='sm:w-36 w-36 break-words text-sm mt-2'>
                 {title}
               </p>
             </a>
