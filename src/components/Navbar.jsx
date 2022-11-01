303c8681fa7504ea493e26ec4ef48784685b38d4
Derrick Ampire <ampire90@gmail.com>
2022-11-01 12:20:07 +0300

    inline styling improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { Search } from './Search';
 
+// component for the navigation bar
 export const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
     <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200'>
@@ -13,7 +14,7 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
                 </p>
             </Link>
             <button type='button' onClick={() => setDarkTheme(!darkTheme)} className='text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg'>
-                {darkTheme ? 'Light Mode ☀️' : 'Dark Mode 🌙'}
+                {darkTheme ? 'Light Mode ☀️' : 'Dark Mode 🌙'}  // toggles between light and dark mode
             </button>
         </div>
         <Search />
